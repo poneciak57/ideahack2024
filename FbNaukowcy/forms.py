70de93b43091cb8a2d824db22e32406b55dd7b1a
fimpro <filipmanijak05@gmail.com>
2024-11-16 23:49:14 +0100

    Adding Projects

--- FbNaukowcy/forms.py
+++ FbNaukowcy/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from common.models import Paper
+from common.models import Paper, Project
 
 class PublicationForm(forms.ModelForm):
     class Meta:
@@ -16,6 +16,37 @@ class PublicationForm(forms.ModelForm):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.user:  # Assign the user to the instance
+            instance.user = self.user
+        if commit:
+            instance.save()
+        return instance
+
+class ProjectForm(forms.ModelForm):
+
+    class Meta:
+        model = Project
+        fields = ['title', 'brief','is_public','content','required_money','type','project_scope','profiles']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Content'}),
+            'brief': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Content'}),
+            'is_public': forms.CheckboxInput(),
+            'type': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Type'}),
+            'profiles': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)  # Get the user from the view
+        super().__init__(*args, **kwargs)
+
+        if user:
+            # Set the initial value for the author field to the logged-in user
+            self.fields['author'] = forms.CharField(initial=user.profile_set,
+                                                    widget=forms.HiddenInput())  # You can make it hidden or display it
+            self.instance.author = user  # Set the author when saving the instance
     def save(self, commit=True):
         instance = super().save(commit=False)
         if self.user:  # Assign the user to the instance
--- FbNaukowcy/views.py
+++ FbNaukowcy/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect, HttpResponse
-from common.models import Paper, Profile
-from .forms import PublicationForm
+from common.models import Paper, Profile, Project
+from .forms import PublicationForm, ProjectForm
+
+
 def publication_list(request):
     publications = Paper.objects.all()
 
     users = Profile.objects.exclude().distinct()#chcemy dawać tylko podobnych użytkowników
 
 
+
     return render(request, 'FbNaukowcy/publication_list.html',
                   {'publications': publications, 'users': users})
 def project_list(request):
-    projects = Paper.objects.all()
+    projects = Project.objects.all()
     users = Profile.objects.exclude().distinct()
     return render(request, 'FbNaukowcy/projects_list.html',
                   {'publications': projects, 'users': users})
@@ -27,7 +30,7 @@ def add_publication(request):
 
 def add_project(request):
     if request.method == 'POST':
-        form = PublicationForm(request.POST,user=request.user)
+        form = ProjectForm(request.POST,user=request.user)
         if form.is_valid():
             form.save()
             return redirect('publication_list')  # Przekierowanie na listę postów
