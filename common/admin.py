908fc61710e68eba668ff4cf3d66bde523c3b66e
SomePersonFromMars <42392219+SomePersonFromMars@users.noreply.github.com>
2024-11-16 21:53:12 +0100

    Fix custom users migrations

--- common/admin.py
+++ common/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Profile, Paper
+from .models import Profile, Paper, CustomUser
 
 admin.site.register(Profile)
 admin.site.register(Paper)
+admin.site.register(CustomUser)
