9e0f783f772851492c7706f5097b04ff85e85e48
SomePersonFromMars <42392219+SomePersonFromMars@users.noreply.github.com>
2024-11-16 20:58:01 +0100

    Add Profile and Paper models

--- common/models.py
+++ common/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 class Profile(models.Model):
+    PROFILE_TYPE_CHOICES = [
+        ('scientist', 'Scientist'),
+        ('businessman', 'Businessman'),
+        ('investor', 'Investor'),
+    ]
+
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     # Can be either 'scientist' or 'businessman' or 'investor'
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=PROFILE_TYPE_CHOICES)
     open_for_contact = models.BooleanField(default=True)
 
     def __str__(self):
@@ -14,9 +20,44 @@ class Paper(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     brief = models.TextField()
-    link = models.CharField(max_length=100)
-    publication_date = models.DateField()
+    link = models.CharField(max_length=200)
+    publication_date = models.DateField(auto_now_add=True)
     embedding = ArrayField(models.FloatField(), blank=True, default=list)
+    def __str__(self):
+        return f'{self.title}'
+
+class Project(models.Model):
+    PROJECT_TYPE_CHOICES = [
+        ('startup', 'Startup'),
+        ('working business', 'Working Business'),
+        ('research', 'Research'),
+    ]
+
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    required_money = models.FloatField()
+    type = models.CharField(max_length=20, choices=PROJECT_TYPE_CHOICES)
+    brief = models.TextField()
+    project_scope = models.TextField()
+    profiles = models.ManyToManyField(Profile, related_name='projects')
 
     def __str__(self):
         return f'{self.title}'
+
+class Invitation(models.Model):
+    INVITATION_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    ]
+
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=100, choices=INVITATION_STATUS_CHOICES)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.sender.user.username} -> {self.receiver.user.username} - {self.status} to {self.project.title}'
