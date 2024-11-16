a663416e471f0f99031700bf90e07f6d1a97dc4d
poneciak <poneciak@gmail.com>
2024-11-17 00:01:36 +0100

    implemented registration

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from .forms import CustomUserRegistrationForm
+from common.models import Profile
 # Login view
 def login_view(request):
     if request.method == 'POST':
@@ -14,6 +15,31 @@ def login_view(request):
         else:
             return render(request, 'auth_app/login.html', {'error': 'Nieprawidłowy login lub hasło'})
     return render(request, 'auth_app/login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            # Save the user
+            user = form.save()
+
+            # Create the profile linked to the user
+            profile_type = form.cleaned_data['profile_type']
+            Profile.objects.create(
+                user=user,
+                type=profile_type,
+                open_for_contact=True  # You can adjust this based on your form or set a default value
+            )
+
+            # Success message
+            # messages.success(request, 'Your account has been created successfully!')
+            return redirect('login')  # Redirect to login or another page after registration
+        # else:
+            # messages.error(request, 'There was an error with your registration. Please check the form.')
+    else:
+        form = CustomUserRegistrationForm()
+
+    return render(request, 'auth_app/register.html', {'form': form})
 
 # Logout view
 def logout_view(request):
