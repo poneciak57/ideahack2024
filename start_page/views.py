c04424525db67998aecf96ebea0da78d7985016e
SomePersonFromMars <42392219+SomePersonFromMars@users.noreply.github.com>
2024-11-17 11:09:35 +0100

    Fix bug on empty database

--- start_page/views.py
+++ start_page/views.py
@@ -3,6 +3,11 @@ from common.models import Profile
 
 def home(request):
     logged_user = request.user
-    is_scientist = logged_user.profile_set.filter(type='scientist').exists()
-    is_businessman = logged_user.profile_set.filter(type='businessman').exists()
+    is_authenticated = logged_user.is_authenticated
+    if is_authenticated:
+        is_scientist = logged_user.profile_set.filter(type='scientist').exists()
+        is_businessman = logged_user.profile_set.filter(type='businessman').exists()
+    else:
+        is_scientist = False
+        is_businessman = False
     return render(request, 'start_page/home.html', context={'is_scientist': is_scientist, 'is_businessman': is_businessman})
